665492e9816249ac4b5a07325d3d59499d7fe8c7
mlegeay2 <46281345+mlegeay2@users.noreply.github.com>
2019-01-01 19:28:35 +0100

    Add files via upload

--- Code/Weather.py
+++ Code/Weather.py
@@ -1,25 +1,32 @@
 import random
-from multiprocessing import Array, Process
+import math
+from multiprocessing import Array, Process, Value
 
-class Weather(Process):
-    def __init__(self):
-        super().__init__()
 
-    def run(self):
-        WeatherTab[0] = random.randrange(-10, 40)
-        WeatherTab[1] = random.randint(1, 3)
+def runWeather(WeatherTab, iteration):
+    #Récupération des anciennes valeurs de WeatherTab avant modification
+    t= WeatherTab[0]
+    alpha = random.randrange(-4, 4)
+    iteration.value= iteration.value +1
+    T= int(math.cos(iteration.value)*t*alpha)  #La nouvelle température est calculée à partir de l'ancienne température, d'un paramètre alpha aléatoire et d'un paramètre w implémenté de 1 à chaque appel de run
+    WeatherTab[0] = T
+    if (T<0) :
+        WeatherTab[1] = random.randint(1, 3) #1 : Soleil    2 : Nuage    3 : Neige
+    else :
+        WeatherTab[1] = random.randint(1, 2)    #il ne peut pas neiger si T>0°
 
 
 if __name__ == "__main__":
-
     WeatherTab = Array('i', range(2))
+    iteration= Value('i', 1)
 
-    meteo = Weather()
+    #initialisation
+    #WeatherTab[0]=21
+
+    meteo = Process(target=runWeather, args=(WeatherTab, iteration))
 
     meteo.start()
-
-    for i in range(0,4):
-        meteo.run()
-        print("Jour",i,": il fait", WeatherTab[0], "degrés celcius et le temps est de", WeatherTab[1])
     meteo.join()
 
+    print(iteration.value)
+    print(WeatherTab[:])
